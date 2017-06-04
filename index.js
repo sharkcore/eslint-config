6dbeb502bc0b803856597eeb51b7b2a28d30c4a4
Mark Larah <mark@larah.me>
2017-06-04 08:35:07 -0700

    init version

---
+++ index.js
@@ -0,0 +1,31 @@
+module.exports = {
+  extends: [
+    'airbnb',
+    'plugin:flowtype/recommended',
+    'prettier',
+    'prettier/flowtype'
+  ],
+  plugins: ['flowtype', 'prettier'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: 'module'
+  },
+  env: {
+    es6: true,
+  },
+  rules: {
+    quotes: [
+      'error',
+      'single',
+      { avoidEscape: true, allowTemplateLiterals: true }
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'all'
+      }
+    ]
+  }
+};
