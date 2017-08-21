e096c9862789182b087a2d0b41942a8e4ada8a10
Mark Larah <mark@larah.me>
2017-08-21 01:39:21 -0700

    [Major] 2.0.1 - Bump to ESLint 4.x.x

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ module.exports = {
   parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 2017,
-    sourceType: 'module'
+    sourceType: 'module',
+    ecmaFeatures: {
+      'jsx': true
+    }
   },
   env: {
     es6: true,
