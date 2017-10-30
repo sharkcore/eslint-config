c194c1c2368eec90c1305f11462741ce527e6c17
Mark Larah <mark@larah.me>
2017-10-29 19:31:51 -0700

    run eslint on the eslint config

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
 module.exports = {
-  extends: [
-    'airbnb',
-    'plugin:flowtype/recommended',
-    'prettier',
-    'prettier/flowtype',
-  ],
-  plugins: ['flowtype', 'prettier'],
-  parser: 'babel-eslint',
-  parserOptions: {
-    ecmaVersion: 2017,
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-      experimentalObjectRestSpread: true,
+    extends: [
+        'airbnb',
+        'plugin:flowtype/recommended',
+        'prettier',
+        'prettier/flowtype',
+    ],
+    plugins: ['flowtype', 'prettier'],
+    parser: 'babel-eslint',
+    parserOptions: {
+        ecmaVersion: 2017,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+            experimentalObjectRestSpread: true,
+        },
     },
-  },
-  env: {
-    es6: true,
-  },
-  rules: {
-    // Enforce indent size of 4 spaces to conform to shark standards
-    indent: ['error', 4],
-    'react/jsx-indent': ['error', 4],
+    env: {
+        es6: true,
+    },
+    rules: {
+        // Enforce indent size of 4 spaces to conform to shark standards
+        indent: ['error', 4],
+        'react/jsx-indent': ['error', 4],
 
-    quotes: [
-      'error',
-      'single',
-      {avoidEscape: true, allowTemplateLiterals: true},
-    ],
-    'prettier/prettier': [
-      'error',
-      {
-        tabWidth: 4,
-        singleQuote: true,
-        trailingComma: 'all',
-      },
-    ],
-  },
+        quotes: [
+            'error',
+            'single',
+            { avoidEscape: true, allowTemplateLiterals: true },
+        ],
+        'prettier/prettier': [
+            'error',
+            {
+                tabWidth: 4,
+                singleQuote: true,
+                trailingComma: 'all',
+            },
+        ],
+    },
 };
