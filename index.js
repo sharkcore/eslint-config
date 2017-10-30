9c3da4732ffa834aec41586840a22625c55b52fa
Mark Larah <mark@larah.me>
2017-10-29 19:27:25 -0700

    updates

--- index.js
+++ index.js
@@ -12,20 +12,26 @@ module.exports = {
     sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
+      experimentalObjectRestSpread: true,
     },
   },
   env: {
     es6: true,
   },
   rules: {
+    // Enforce indent size of 4 spaces to conform to shark standards
+    indent: ['error', 4],
+    'react/jsx-indent': ['error', 4],
+
     quotes: [
       'error',
       'single',
-      { avoidEscape: true, allowTemplateLiterals: true },
+      {avoidEscape: true, allowTemplateLiterals: true},
     ],
     'prettier/prettier': [
       'error',
       {
+        tabWidth: 4,
         singleQuote: true,
         trailingComma: 'all',
       },
